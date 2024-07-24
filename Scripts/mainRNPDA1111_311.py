d4c086469fb3b61e881eaef8c1d72141af8a8718
utmostmick0 <mick0mobile@gmail.com>
2024-07-24 12:51:35 +0800

    Add files via upload

--- Scripts/mainRNPDA1111_311.py
+++ Scripts/mainRNPDA1111_311.py
@@ -21,10 +21,10 @@ def Deps(force_reinstall):
 
     if not force_reinstall and os.path.exists('/usr/local/lib/python3.11/dist-packages/gradio'):
         ntbk()
-        os.environ['TORCH_HOME'] = '/notebooks/cache/torch'
+        os.environ['TORCH_HOME'] = '/workspace/cache/torch'
         os.environ['PYTHONWARNINGS'] = 'ignore'
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        print('[1;32mModules and notebooks updated, dependencies already installed')
+        print('[1;32mModules and workspace updated, dependencies already installed')
 
     else:
         call("pip install --root-user-action=ignore --no-deps -q accelerate==0.12.0", shell=True, stdout=open('/dev/null', 'w'))
@@ -32,8 +32,8 @@ def Deps(force_reinstall):
         ntbk()
         if not os.path.exists('/models'):
             call('mkdir /models', shell=True)
-        if not os.path.exists('/notebooks/models'):
-            call('ln -s /models /notebooks', shell=True)
+        if not os.path.exists('/workspace/models'):
+            call('ln -s /models /workspace', shell=True)
         if os.path.exists('/deps'):
             call("rm -r /deps", shell=True)
         call('mkdir /deps', shell=True)
@@ -44,16 +44,16 @@ def Deps(force_reinstall):
         call('dpkg -i *.deb', shell=True, stdout=open('/dev/null', 'w'))
         depsinst("https://github.com/utmostmick0/sd_dependencies/blob/f2216938549c58f6d16363cdbe8650960839bc7e/rnpddeps-t2.tar.zst", "/deps/pnpddeps-t2.tar.zst")
         call('tar -C / --zstd -xf npddeps-t2.tar.zst', shell=True, stdout=open('/dev/null', 'w'))
-        os.chdir('/notebooks')
+        os.chdir('/workspace')
         call("git clone --depth 1 -q --branch main https://github.com/utmostmick0/diffusers /diffusers", shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-        os.environ['TORCH_HOME'] = '/notebooks/cache/torch'
+        os.environ['TORCH_HOME'] = '/workspace/cache/torch'
         os.environ['PYTHONWARNINGS'] = 'ignore'
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
         call("sed -i 's@text = _formatwarnmsg(msg)@text =\"\"@g' /usr/lib/python3.11/warnings.py", shell=True)
-        if not os.path.exists('/notebooks/diffusers'):
-            call('ln -s /diffusers /notebooks', shell=True)
+        if not os.path.exists('/workspace/diffusers'):
+            call('ln -s /diffusers /workspace', shell=True)
         call("rm -r /deps", shell=True)
-        os.chdir('/notebooks')
+        os.chdir('/workspace')
         clear_output()
 
         done()
@@ -112,40 +112,40 @@ def dwn(url, dst, msg):
 
 def ntbk():
 
-    os.chdir('/notebooks')
+    os.chdir('/workspace')
     if not os.path.exists('Latest_Notebooks'):
         call('mkdir Latest_Notebooks', shell=True)
     else:
         call('rm -r Latest_Notebooks', shell=True)
         call('mkdir Latest_Notebooks', shell=True)
-    os.chdir('/notebooks/Latest_Notebooks')
+    os.chdir('/workspace/Latest_Notebooks')
     call('wget -q -i https://github.com/utmostmick0/RNPD/blob/eb562416cbb951e6f665638b9a605d7201640814/Notebooks.txt', shell=True)
     call('rm Notebooks.txt', shell=True)
-    os.chdir('/notebooks')
+    os.chdir('/workspace')
 
 
 
 def repo():
 
     print('[1;32mInstalling/Updating the repo...')
-    os.chdir('/notebooks')
-    if not os.path.exists('/notebooks/sd/stablediffusiond'): #reset later
+    os.chdir('/workspace')
+    if not os.path.exists('/workspace/sd/stablediffusiond'): #reset later
        call('wget -q -O sd_mrep.tar.zst https://github.com/utmostmick0/dependencies/blob/3d4911654a6939e676f4353806b16beeb08c89fd/sd_mrep.tar.zst', shell=True)
        call('tar --zstd -xf sd_mrep.tar.zst', shell=True)
        call('rm sd_mrep.tar.zst', shell=True)        
 
-    os.chdir('/notebooks/sd')
+    os.chdir('/workspace/sd')
     if not os.path.exists('stable-diffusion-webui'):
         call('git clone -q --depth 1 --branch master https://github.com/AUTOMATIC1111/stable-diffusion-webui', shell=True)
 
-    os.chdir('/notebooks/sd/stable-diffusion-webui/')
+    os.chdir('/workspace/sd/stable-diffusion-webui/')
     call('git reset --hard', shell=True, stdout=open('/dev/null', 'w'))
     print('[1;32m')
     call('git checkout master', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
     call('git pull', shell=True, stdout=open('/dev/null', 'w'))
-    os.makedirs('/notebooks/sd/stable-diffusion-webui/repositories', exist_ok=True)
-    call('git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-assets /notebooks/sd/stable-diffusion-webui/repositories/stable-diffusion-webui-assets', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-    os.chdir('/notebooks')
+    os.makedirs('/workspace/sd/stable-diffusion-webui/repositories', exist_ok=True)
+    call('git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-assets /workspace/sd/stable-diffusion-webui/repositories/stable-diffusion-webui-assets', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    os.chdir('/workspace')
     clear_output()
     done()
 
@@ -161,10 +161,10 @@ def mdls(Original_Model_Version, Path_to_MODEL, MODEL_LINK, Temporary_Storage):
     src=getsrc(MODEL_LINK)
 
 
-    call('ln -s /datasets/stable-diffusion-classic/SDv1.5.ckpt /notebooks/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-    call('ln -s /datasets/stable-diffusion-v2-1-base-diffusers/stable-diffusion-2-1-base/v2-1_512-nonema-pruned.safetensors /notebooks/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-    call('ln -s /datasets/stable-diffusion-v2-1/stable-diffusion-2-1/v2-1_768-nonema-pruned.safetensors /notebooks/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-    call('ln -s /datasets/stable-diffusion-xl/sd_xl_base_1.0.safetensors /notebooks/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    call('ln -s /datasets/stable-diffusion-classic/SDv1.5.ckpt /workspace/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    call('ln -s /datasets/stable-diffusion-v2-1-base-diffusers/stable-diffusion-2-1-base/v2-1_512-nonema-pruned.safetensors /workspace/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    call('ln -s /datasets/stable-diffusion-v2-1/stable-diffusion-2-1/v2-1_768-nonema-pruned.safetensors /workspace/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
+    call('ln -s /datasets/stable-diffusion-xl/sd_xl_base_1.0.safetensors /workspace/sd/stable-diffusion-webui/models/Stable-diffusion', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
 
     if Path_to_MODEL !='':
       if os.path.exists(str(Path_to_MODEL)):
@@ -180,7 +180,7 @@ def mdls(Original_Model_Version, Path_to_MODEL, MODEL_LINK, Temporary_Storage):
          if Temporary_Storage:
             model=f'/models/{modelname}'
          else:
-            model=f'/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
+            model=f'/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
          if not os.path.exists(model):
             dwn(MODEL_LINK, model, 'Downloading the custom model')
             clear_output()
@@ -191,7 +191,7 @@ def mdls(Original_Model_Version, Path_to_MODEL, MODEL_LINK, Temporary_Storage):
          if Temporary_Storage:
             model=f'/models/{modelname}'
          else:
-            model=f'/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
+            model=f'/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
          if not os.path.exists(model):
             gdown.download(url=MODEL_LINK, output=model, quiet=False, fuzzy=True)
             clear_output()
@@ -202,7 +202,7 @@ def mdls(Original_Model_Version, Path_to_MODEL, MODEL_LINK, Temporary_Storage):
          if Temporary_Storage:
             model=f'/models/{modelname}'
          else:
-            model=f'/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
+            model=f'/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/{modelname}'
          if not os.path.exists(model):
             gdown.download(url=MODEL_LINK, output=model, quiet=False, fuzzy=True)
             clear_output()
@@ -217,24 +217,24 @@ def mdls(Original_Model_Version, Path_to_MODEL, MODEL_LINK, Temporary_Storage):
 
     else:
         if Original_Model_Version == "v1.5":
-           model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/SDv1.5.ckpt"
+           model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/SDv1.5.ckpt"
            print('[1;32mUsing the original V1.5 model')
         elif Original_Model_Version == "v2-512":
-            model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/v2-1_512-nonema-pruned.safetensors"
+            model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/v2-1_512-nonema-pruned.safetensors"
             print('[1;32mUsing the original V2-512 model')
         elif Original_Model_Version == "v2-768":
-           model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/v2-1_768-nonema-pruned.safetensors"
+           model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/v2-1_768-nonema-pruned.safetensors"
            print('[1;32mUsing the original V2-768 model')
         elif Original_Model_Version == "SDXL":
-            model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion/sd_xl_base_1.0.safetensors"
+            model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion/sd_xl_base_1.0.safetensors"
             print('[1;32mUsing the original SDXL model')
         else:
-            model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion"
+            model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion"
             print('[1;31mWrong model version, try again')
     try:
         model
     except:
-        model="/notebooks/sd/stable-diffusion-webui/models/Stable-diffusion"
+        model="/workspace/sd/stable-diffusion-webui/models/Stable-diffusion"
 
     return model
 
@@ -248,13 +248,13 @@ def loradwn(LoRA_LINK):
     if LoRA_LINK=='':
         print('[1;33mNothing to do')
     else:
-        os.makedirs('/notebooks/sd/stable-diffusion-webui/models/Lora', exist_ok=True)
+        os.makedirs('/workspace/sd/stable-diffusion-webui/models/Lora', exist_ok=True)
 
         src=getsrc(LoRA_LINK)
 
         if src=='civitai':
             modelname=get_name(LoRA_LINK, False)
-            loramodel=f'/notebooks/sd/stable-diffusion-webui/models/Lora/{modelname}'
+            loramodel=f'/workspace/sd/stable-diffusion-webui/models/Lora/{modelname}'
             if not os.path.exists(loramodel):
               dwn(LoRA_LINK, loramodel, 'Downloading the LoRA model')
               clear_output()
@@ -262,7 +262,7 @@ def loradwn(LoRA_LINK):
               print('[1;33mModel already exists')
         elif src=='gdrive':
             modelname=get_name(LoRA_LINK, True)
-            loramodel=f'/notebooks/sd/stable-diffusion-webui/models/Lora/{modelname}'
+            loramodel=f'/workspace/sd/stable-diffusion-webui/models/Lora/{modelname}'
             if not os.path.exists(loramodel):
               gdown.download(url=LoRA_LINK, output=loramodel, quiet=False, fuzzy=True)
               clear_output()
@@ -270,7 +270,7 @@ def loradwn(LoRA_LINK):
               print('[1;33mModel already exists')
         else:
             modelname=os.path.basename(LoRA_LINK)
-            loramodel=f'/notebooks/sd/stable-diffusion-webui/models/Lora/{modelname}'
+            loramodel=f'/workspace/sd/stable-diffusion-webui/models/Lora/{modelname}'
             if not os.path.exists(loramodel):
               gdown.download(url=LoRA_LINK, output=loramodel, quiet=False, fuzzy=True)
               clear_output()
@@ -297,17 +297,17 @@ def CN(ControlNet_Model, ControlNet_XL_Model):
           print(f"[1;32mThe model {filename} already exists[0m")    
 
     wrngv1=False
-    os.chdir('/notebooks/sd/stable-diffusion-webui/extensions')
+    os.chdir('/workspace/sd/stable-diffusion-webui/extensions')
     if not os.path.exists("sd-webui-controlnet"):
       call('git clone https://github.com/Mikubill/sd-webui-controlnet.git', shell=True)
-      os.chdir('/notebooks')
+      os.chdir('/workspace')
     else:
       os.chdir('sd-webui-controlnet')
       call('git reset --hard', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
       call('git pull', shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-      os.chdir('/notebooks')
+      os.chdir('/workspace')
 
-    mdldir="/notebooks/sd/stable-diffusion-webui/extensions/sd-webui-controlnet/models"
+    mdldir="/workspace/sd/stable-diffusion-webui/extensions/sd-webui-controlnet/models"
     for filename in os.listdir(mdldir):
       if "_sd14v1" in filename:
         renamed = re.sub("_sd14v1", "-fp16", filename)
@@ -322,7 +322,7 @@ def CN(ControlNet_Model, ControlNet_XL_Model):
         mdllnk_XL = d.read().splitlines()
     call('rm CN_models.txt CN_models_XL.txt', shell=True)
     
-    os.chdir('/notebooks')
+    os.chdir('/workspace')
 
     if ControlNet_Model == "All" or ControlNet_Model == "all" :     
       for lnk in mdllnk:
@@ -396,13 +396,13 @@ def sdui(User, Password, model):
       sys.stdout.write(line)
 
      
-    os.chdir('/notebooks/sd/stable-diffusion-webui/modules')
+    os.chdir('/workspace/sd/stable-diffusion-webui/modules')
     
-    call("sed -i 's@possible_sd_paths =.*@possible_sd_paths = [\"/notebooks/sd/stablediffusion\"]@' /notebooks/sd/stable-diffusion-webui/modules/paths.py", shell=True)
-    call("sed -i 's@\.\.\/@src/@g' /notebooks/sd/stable-diffusion-webui/modules/paths.py", shell=True)
-    call("sed -i 's@src\/generative-models@generative-models@g' /notebooks/sd/stable-diffusion-webui/modules/paths.py", shell=True)
+    call("sed -i 's@possible_sd_paths =.*@possible_sd_paths = [\"/workspace/sd/stablediffusion\"]@' /workspace/sd/stable-diffusion-webui/modules/paths.py", shell=True)
+    call("sed -i 's@\.\.\/@src/@g' /workspace/sd/stable-diffusion-webui/modules/paths.py", shell=True)
+    call("sed -i 's@src\/generative-models@generative-models@g' /workspace/sd/stable-diffusion-webui/modules/paths.py", shell=True)
 
-    os.chdir('/notebooks/sd/stable-diffusion-webui')
+    os.chdir('/workspace/sd/stable-diffusion-webui')
     clear_output()
 
     if model=="":
