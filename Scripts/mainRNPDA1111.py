92f43816b42eedea0bac40280f22b737585ba8c4
utmostmick0 <mick0mobile@gmail.com>
2024-07-24 18:30:16 +0800

    Update mainRNPDA1111.py

--- Scripts/mainRNPDA1111.py
+++ Scripts/mainRNPDA1111.py
@@ -42,7 +42,7 @@ def repo():
     os.chdir('sd')
 
     if not os.path.exists('stablediffusiond'):
-        subprocess.call('wget -q -O sd_mrep.tar.zst https://github.com/utmostmick0/resolve/main/dependencies/sd_mrep.tar.zst', shell=True)
+        subprocess.call('wget -q -O npddeps-t2.tar.zst https://github.com/utmostmick0/sd_dependencies/raw/main/r', shell=True)
         # Use tar -xf to handle various formats
         subprocess.call('tar -xf sd_mrep.tar.zst', shell=True)
         subprocess.call('rm sd_mrep.tar.zst', shell=True)
